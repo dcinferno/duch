0f63351a61c1101d499e014f052f2e79314ab575
derek-charles <52976014+derek-charles@users.noreply.github.com>
2025-12-23 07:23:38 -0600

    fix url

--- src/app/api/videos/route.js
+++ src/app/api/videos/route.js
@@ -188,7 +188,7 @@ export async function POST(request) {
       price: Number(price),
       creatorName: creator.name,
       socialMediaUrl,
-      url: normalizePath(url),
+      url,
       fullKey: normalizeFullKey(fullKey),
       tags,
     });
