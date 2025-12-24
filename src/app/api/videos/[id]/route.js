0d5efb6d7cec0fc43a680617d5da08f831e75549
derek-charles <52976014+derek-charles@users.noreply.github.com>
2025-12-24 05:33:33 -0600

    applyDiscount to Video

--- src/app/api/videos/[id]/route.js
+++ src/app/api/videos/[id]/route.js
@@ -5,6 +5,34 @@ import { connectToDB } from "../../../../lib/mongodb.js";
 import Videos from "../../../../models/videos";
 import Creators from "../../../../models/creators";
 import { fetchActiveDiscounts } from "../../../../lib/fetchActiveDiscounts";
+function applyDiscountToVideo(video, discounts) {
+  const basePrice = Number(video.price) || 0;
+
+  if (basePrice <= 0) {
+    return { basePrice, finalPrice: basePrice, discount: null };
+  }
+
+  const creatorKey = video.creatorName?.trim().toLowerCase();
+
+  const discount = discounts.creators?.[creatorKey] || discounts.global || null;
+
+  if (!discount) {
+    return { basePrice, finalPrice: basePrice, discount: null };
+  }
+
+  const finalPrice = discount.percentOff
+    ? basePrice * (1 - discount.percentOff / 100)
+    : basePrice;
+
+  return {
+    basePrice,
+    finalPrice: Number(finalPrice.toFixed(2)),
+    discount: {
+      name: discount.name,
+      percentOff: discount.percentOff,
+    },
+  };
+}
 
 export async function GET(req, { params }) {
   await connectToDB();
